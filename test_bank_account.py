8440735a9f2692eabbcd54b792d7c12af69b131e
eugenesaringan4-debug <eugenesaringan4@gmail.com>
2026-08-23 10:52:39 +0800

    bank_account.py

---
+++ test_bank_account.py
@@ -0,0 +1,28 @@
+class BankAccount:
+    def __init__(self, owner, balance):
+        if balance < 0:
+            raise ValueError("Opening balance cannot be negative")
+
+        self.owner = owner
+        self._balance = balance
+
+    def get_balance(self):
+        return self._balance
+
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Deposit must be greater than zero")
+
+        self._balance += amount
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            raise ValueError("Withdrawal must be greater than zero")
+
+        if amount > self._balance:
+            raise ValueError("Insufficient balance")
+
+        self._balance -= amount
+
+    def __str__(self):
+        return f"{self.owner}: {self._balance:.2f}"
